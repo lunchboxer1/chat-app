a98b19556364d94690c6afa2854eaca259462e18
Eric Parks <eparks1860@gmail.com>
2022-01-23 14:52:48 -0500

    Init commit

---
+++ src/app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const path = require('path');
+const http = require('http');
+const socketio = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+const publicDirectoryPath = path.join(__dirname, '../public');
+
+app.use(express.static(publicDirectoryPath));
+
+let count = 0;
+
+io.on('connection', (socket) => {
+  console.log('New web socket connection.');
+
+  socket.emit('countUpdated', count);
+
+  socket.on('increment', () => {
+    count += 1;
+
+    // socket.emit('countUpdated', count);
+    io.emit('countUpdated', count)
+  });
+});
+
+app.get('/test', (req, res) => {
+  // res.render('index');
+  res.send({ msg: 'Heyo' });
+});
+
+module.exports = { app, server };
